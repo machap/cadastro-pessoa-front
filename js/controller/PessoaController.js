d01871a669f4fd8243139f8d32e25f53d395934f
machap <paulinhodj10@gmail.com>
2019-01-30 16:35:20 -0300

    Carregando lista de pessoas e incluindo na tabela

--- js/controller/PessoaController.js
+++ js/controller/PessoaController.js
@@ -1,16 +1,41 @@
 class PessoaController {
     constructor() {
         this._listaPessoas = new ListaPessoas();
+
+        this.importaPessoas();
     }
 
     importaPessoas() {
-        let service = new PessoaService();
 
+        let service = new PessoaService();
         let promise = service.obterTodos();
+
         promise.then(pessoas => {
-            // pessoas.forEach(pessoa => this._listaPessoas.adiciona(pessoa));
-            pessoas.forEach(pessoa => console.log(pessoa));
+            pessoas.forEach(pessoa => {
+                this._listaPessoas.adiciona(pessoa);
+            });
+
+            this._listaPessoas.pessoas.forEach(pessoa => {
+                let tbody = document.querySelector('table tbody');
+                let tr = document.createElement('tr');
+                let tdId = document.createElement('td');
+                let tdNome = document.createElement('td');
+                let tdIdade = document.createElement('td');
+                let tdEmail = document.createElement('td');
+
+                tdId.textContent = pessoa.id;
+                tdNome.textContent = pessoa.nome;
+                tdIdade.textContent = pessoa.idade;
+                tdEmail.textContent = pessoa.email;
+
+                tr.appendChild(tdId);
+                tr.appendChild(tdNome);
+                tr.appendChild(tdIdade);
+                tr.appendChild(tdEmail);
+
+                tbody.appendChild(tr);
+            });
+
         }).catch(erro => console.log(erro));
     }
-    
 }
