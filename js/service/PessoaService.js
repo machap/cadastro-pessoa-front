73299cb9b79ba2cd714b33eb7fc1ca38e9057231
machap <paulinhodj10@gmail.com>
2019-01-30 12:52:48 -0300

    Criando servico de requisicao com metodo GET

--- js/service/PessoaService.js
+++ js/service/PessoaService.js
@@ -1,3 +1,18 @@
 class PessoaService {
-    
+    constructor() {
+        this._http = new HttpService();
+    }
+
+    obterTodos() {
+        return new Promise((resolve, reject) => {
+            this._http.get('https://apirest-pessoas.herokuapp.com/api/pessoas')
+                .then(pessoas => {
+                    resolve(pessoas.map(objeto => new Pessoa(objeto.id, objeto.nome, objeto.idade, objeto.email)));
+                }).catch(erro => {
+                console.log(erro);
+                reject('Não foi possivel obter a lista de pessoas cadastradas');
+            });
+        });
+    }
+
 }
