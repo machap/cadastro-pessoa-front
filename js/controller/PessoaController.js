793300ddfee159cba9b84666ece46a7f5e1b4f89
machap <paulinhodj10@gmail.com>
2019-01-30 12:53:30 -0300

    Importando dados de pessoas no controler

--- js/controller/PessoaController.js
+++ js/controller/PessoaController.js
@@ -1,3 +1,16 @@
 class PessoaController {
+    constructor() {
+        this._listaPessoas = new ListaPessoas();
+    }
+
+    importaPessoas() {
+        let service = new PessoaService();
+
+        let promise = service.obterTodos();
+        promise.then(pessoas => {
+            // pessoas.forEach(pessoa => this._listaPessoas.adiciona(pessoa));
+            pessoas.forEach(pessoa => console.log(pessoa));
+        }).catch(erro => console.log(erro));
+    }
     
 }
